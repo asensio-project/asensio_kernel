981ba8d93185815e90bd7368342a92f6d224e381
Juanjo Asensio García <asengar2009@gmail.com>
2023-01-30 16:18:49 +0100

    Use the UPSTREAM version of the bootloader and use the BootConfig struct

--- build.rs
+++ build.rs
@@ -1,3 +1,5 @@
+use bootloader_boot_config::LevelFilter;
+use bootloader::BootConfig;
 use std::path::PathBuf;
 
 fn main() {
@@ -7,15 +9,21 @@ fn main() {
     // https://doc.rust-lang.org/nightly/cargo/reference/unstable.html#artifact-dependencies
     let kernel = PathBuf::from(std::env::var_os("CARGO_BIN_FILE_KERNEL_kernel").unwrap());
 
+    // create the BootConfig
+    let mut boot_config: BootConfig = Default::default();
+    boot_config.log_level = bootloader_boot_config::LevelFilter::Error;
+
     // create an UEFI disk image (optional)
     let uefi_path = out_dir.join("asensio_kernel-x86_64-uefi.img");
     bootloader::UefiBoot::new(&kernel)
+        .set_boot_config(&boot_config)
         .create_disk_image(&uefi_path)
         .unwrap();
 
     // create a BIOS disk image
     let bios_path = out_dir.join("asensio_kernel-x86_64-bios.img");
     bootloader::BiosBoot::new(&kernel)
+        .set_boot_config(&boot_config)
         .create_disk_image(&bios_path)
         .unwrap();
 
